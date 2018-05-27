77906a611c38cfacddee5b6dea46ce39e8b4c74e
Son Luong Ngoc (Lazada Group) <son.luongngoc@lazada.com>
2018-05-27 20:29:01 +0700

    Applied DRY with model

--- model/user.go
+++ model/user.go
@@ -2,15 +2,13 @@ package model
 
 type (
 	NewUser struct {
-		Name    string `json:"name"`
-		Email   string `json:"email"`
-		Website string `json:"website" required:"false"`
+		Name    string `json:"name" db:"username"`
+		Email   string `json:"email" db:"email"`
+		Website string `json:"website" db:"website" required:"false"`
 	}
 
 	User struct {
 		ID      int32  `json:"id" db:"id"`
-		Name    string `json:"name" db:"username"`
-		Email   string `json:"email" db:"email"`
-		Website string `json:"website" db:"website"`
+		NewUser
 	}
 )
